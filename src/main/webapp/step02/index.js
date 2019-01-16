c724cf6e628d21446796b958a2075d39dc07312f
LTH <leeth4115@naver.com>
2019-01-16 18:58:02 +0900

    JavaScript data Type

---
+++ src/main/webapp/step02/index.js
@@ -0,0 +1,17 @@
+// String
+const whatString = "String";
+
+// Boolean
+const whatBoolean = true;
+
+// Number
+const whatNumber = 666;
+
+// Float
+const whatFloat =33.1;
+
+
+console.log(whatString);
+console.log(whatBoolean);
+console.log(whatNumber);
+console.log(whatFloat);
