426fff93ccb75988e08bbe6d177d95aca585c3e4
LTH <leeth4115@naver.com>
2019-01-19 21:00:52 +0900

    기본 동작 function 만들어 보기

---
+++ src/main/webapp/step05/index.js
@@ -0,0 +1,12 @@
+// console = object, .log = function
+// console.log, aleart = built-in funtion 내장함수
+console.log(console);
+console.log("Hi");
+
+// 어떻게 우리는 function을 만들 수 있을까?
+
+function sayHello(anything, age){
+	console.log('Hello '+anything, age);
+}
+
+sayHello("Mandarine",12);
